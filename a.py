ebb7297ee7f078e3425bf7c43361ea1ffb9eb640
michaelsanchez9351 <michaelsanchez@outlook.com>
2025-03-05 11:07:01 -0600

    tightened function defintion

--- a.py
+++ a.py
@@ -1,13 +1,10 @@
-print('Hello FedZ world!')
+sampleManifest = [2.3, 5, 9.2, 2, 1, 16, 30, 1, 18, 97, 44, 13, 5, 5, 5, 1, 5]
 
-def sumLoadWeight(n):
+def sumLoadWeight(n, weight = 0):
     """Calculates total weight of objects in given list"""
-    weight = 0
     for x in range(len(n)):
         weight = weight + n[x]
     return(weight)
 
-
-sampleManifest = [2.3, 5, 9.2, 2, 1, 16, 30, 1, 18, 97, 44, 13, 5, 5, 5, 1, 5]
 
 print(sumLoadWeight(sampleManifest))
